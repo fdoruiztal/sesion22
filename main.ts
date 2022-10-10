a361cdd25dc623f37791a1e536efb03d69fa4246
fdoruiztal <112428834+fdoruiztal@users.noreply.github.com>
2022-10-10 12:36:52 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,29 @@
-input.onButtonPressed(Button.A, function () {
-    carlos.move(-1)
-})
-input.onButtonPressed(Button.AB, function () {
-    carlos.change(LedSpriteProperty.Y, 1)
-})
-input.onButtonPressed(Button.B, function () {
-    carlos.move(1)
-})
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    carlos.change(LedSpriteProperty.Y, -1)
-})
-let carlos: game.LedSprite = null
-carlos = game.createSprite(0, 2)
+let clark = game.createSprite(0, 2)
+let lex = game.createSprite(randint(1, 4), randint(0, 4))
 basic.forever(function () {
-	
+    while (input.buttonIsPressed(Button.AB)) {
+        clark.change(LedSpriteProperty.Y, 1)
+        basic.pause(100)
+    }
+    while (input.buttonIsPressed(Button.A)) {
+        clark.change(LedSpriteProperty.X, -1)
+        basic.pause(100)
+    }
+    while (input.buttonIsPressed(Button.B)) {
+        clark.change(LedSpriteProperty.X, 1)
+        basic.pause(100)
+    }
+    while (input.logoIsPressed()) {
+        clark.change(LedSpriteProperty.Y, -1)
+        basic.pause(100)
+    }
+    if (clark.isTouching(lex)) {
+        for (let index = 0; index < 3; index++) {
+            basic.showIcon(IconNames.Yes)
+            basic.pause(500)
+            basic.clearScreen()
+        }
+        music.playMelody("C D E F G A B B ", 120)
+        game.gameOver()
+    }
 })
